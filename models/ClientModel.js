3cb17276e2466d7c4ae568c2d6b1118ebd6e51e9
Priyanshu Kumar <priyanshukumarweb3@gmail.com>
2025-05-03 15:49:54 +0530

    changes

--- models/ClientModel.js
+++ models/ClientModel.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
 const ClientSchema = new mongoose.Schema({
-    signUpVia: {
-        type: Object,
-        required: true,
-    }
+  signUpVia: {
+    type: Object,
+    required: true,
+  },
+  agents: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Agent'
+  }]
 });
 
 const Client = mongoose.model("Client", ClientSchema, "Client");
