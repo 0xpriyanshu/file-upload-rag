5e9b82deabcadb304454b150ef782b084e760fce
Ashwin1011 <p.ashwinsingh@gmail.com>
2025-05-06 12:25:27 +0530

    asd

--- models/AgentModel.js
+++ models/AgentModel.js
@@ -9,10 +9,6 @@ const AgentSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    documentCollectionId: {
-        type: String,
-        required: true,
-    },
     documents: [{
         documentId: {
             type: String,
