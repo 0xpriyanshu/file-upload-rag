1a43a8623f9882f2752a103325f715ee99503cd1
Ashwin1011 <p.ashwinsingh@gmail.com>
2025-06-07 13:01:05 +0530

    asd

--- connections/websocketManager.js
+++ connections/websocketManager.js
@@ -64,6 +64,7 @@ class WebSocketManager {
     // Send to specific client
     sendToClient(clientId, message) {
         const ws = this.clients.get(clientId);
+        console.log("ws", ws);
         if (ws && ws.readyState === WebSocketServer.OPEN) {
             ws.send(JSON.stringify(message));
         }
